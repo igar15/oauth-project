42f630778bb7a955357011d22ab12bab525e863b
Igor Shlyakhtenkov <ishlyakhtenkov@yandex.ru>
2022-04-19 14:27:28 +0300

    Implement User Storage SPI (part 1)

---
+++ user-storage-provider/src/main/java/ru/javaprojects/userstorageprovider/UserStorageProviderApplication.java
@@ -0,0 +1,13 @@
+package ru.javaprojects.userstorageprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserStorageProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserStorageProviderApplication.class, args);
+    }
+
+}
---
+++ user-storage-provider/src/main/java/ru/javaprojects/userstorageprovider/UsersApiService.java
@@ -0,0 +1,14 @@
+package ru.javaprojects.userstorageprovider;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+//@Produces(MediaType.APPLICATION_JSON)
+public interface UsersApiService {
+
+    @GET
+    @Path("/{username}")
+    User getUserDetails(@PathParam("username") String username);
+}
