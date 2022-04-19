20b54bc6bb8e02a2f106306aecddb6ed52823766
Igor Shlyakhtenkov <ishlyakhtenkov@yandex.ru>
2022-04-19 11:45:20 +0300

    Add JS Web Client

---
+++ js-web-client/src/main/java/ru/javaprojects/jswebclient/JsWebClientApplication.java
@@ -0,0 +1,13 @@
+package ru.javaprojects.jswebclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsWebClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsWebClientApplication.class, args);
+    }
+
+}
---
+++ js-web-client/src/test/java/ru/javaprojects/jswebclient/JsWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javaprojects.jswebclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsWebClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
